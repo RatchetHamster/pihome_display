5fe73ad33f9aff13e15f3dd3f3a3e8d047530ace
RatchetHamster <94993719+RatchetHamster@users.noreply.github.com>
2026-01-20 12:19:50 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.core.window import Window
-
-Window.fullscreen = True
-Window.borderless = True
-
-class TestApp(App):
-    def build(self):
-        return Label(text="HELLO", font_size=40)
-
-TestApp().run()
+print("hello world")
